3c2832e165257f986bf2f32d33fe9d52ebedac7d
AndreaGennaioli <63359650+AndreaGennaioli@users.noreply.github.com>
2020-11-10 13:34:01 +0100

    Delete request.js

--- request.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-
-fetch('https://captchaapi.herokuapp.com/generator').then(async (response) => {
-	var data = JSON.parse(await response.text());
-	var image_url = data.image; // the captcha image url (200x100px)
-	var key = data.key; // the captcha key in string
-});
